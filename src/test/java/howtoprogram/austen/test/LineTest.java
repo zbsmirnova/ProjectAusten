93ebc99dc49badbf665d24fa2fdc87fef29609ef
TimLavers <tim@grandtestauto.org>
2016-03-08 18:31:04 +1100

    Second unit test implemented.

--- src/test/java/howtoprogram/austen/test/LineTest.java
+++ src/test/java/howtoprogram/austen/test/LineTest.java
@@ -4,10 +4,20 @@ import howtoprogram.austen.Line;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class LineTest {
     @Test
     public void testEmpty() {
         Line line = new Line("");
         Assert.assertEquals(0, line.words().size());
+    }
+
+    @Test
+    public void testOneWord() {
+        Line line = new Line("hello");
+        List<String> words = line.words();
+        Assert.assertEquals(1, words.size());
+        Assert.assertEquals("hello", words.get(0));
     }
 }
