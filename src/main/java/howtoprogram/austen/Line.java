87d079336dd23f6a6770e7b387f11944c7fcc8e5
TimLavers <tim@grandtestauto.org>
2016-03-08 19:11:27 +1100

    First cut at implementation.

--- src/main/java/howtoprogram/austen/Line.java
+++ src/main/java/howtoprogram/austen/Line.java
@@ -10,7 +10,20 @@ public class Line {
     private List<String> words = new ArrayList<>();
 
     public Line(String text) {
-
+        StringBuilder wordBuilder = new StringBuilder();
+        int length = text.length();
+        String lowerCase = text.toLowerCase();
+        for (int i=0; i<length; i++) {
+            char c = text.charAt(i);
+            if (c == ' ') {
+                String finishedWord = wordBuilder.toString();
+                words.add(finishedWord);
+                wordBuilder = new StringBuilder();
+            } else {
+                wordBuilder.append(lowerCase.charAt(i));
+            }
+        }
+        words.add(wordBuilder.toString());
     }
 
     public List<String> words() {
