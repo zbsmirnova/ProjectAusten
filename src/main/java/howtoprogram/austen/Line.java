fd3a09fae70abee8914849e40398be801f08c2f7
TimLavers <tim@grandtestauto.org>
2016-03-08 18:27:43 +1100

    One unit test implemented

--- src/main/java/howtoprogram/austen/Line.java
+++ src/main/java/howtoprogram/austen/Line.java
@@ -1,7 +1,19 @@
 package howtoprogram.austen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents a line of text read in from a book.
  */
 public class Line {
+    private List<String> words = new ArrayList<>();
+
+    public Line(String text) {
+
+    }
+
+    public List<String> words() {
+        return words;
+    }
 }
--- src/test/java/howtoprogram/austen/test/LineTest.java
+++ src/test/java/howtoprogram/austen/test/LineTest.java
@@ -1,10 +1,13 @@
 package howtoprogram.austen.test;
 
+import howtoprogram.austen.Line;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LineTest {
     @Test
-    public void test1() {
-
+    public void testEmpty() {
+        Line line = new Line("");
+        Assert.assertEquals(0, line.words().size());
     }
 }
