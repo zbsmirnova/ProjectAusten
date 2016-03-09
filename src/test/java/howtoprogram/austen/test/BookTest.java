7e949e5ab5aacb21a3c0a1a71032936df466b2ea
TimLavers <tim@grandtestauto.org>
2016-03-09 21:00:57 +1100

    Two tests for Book.

--- src/test/java/howtoprogram/austen/test/BookTest.java
+++ src/test/java/howtoprogram/austen/test/BookTest.java
@@ -11,6 +11,21 @@ import java.nio.file.Paths;
 public class BookTest {
 
     @Test
+    public void horrorStory() throws IOException {
+        Book book = new Book(Paths.get("src/test/resources/books/HorrorStory.txt"));
+        //This book has just the single line "It was a dark and stormy night"
+        Histogram histogram = book.histogram();
+        Assert.assertEquals(7, histogram.allWords().size());
+        Assert.assertTrue(histogram.allWords().contains("it"));
+        Assert.assertTrue(histogram.allWords().contains("was"));
+        Assert.assertTrue(histogram.allWords().contains("a"));
+        Assert.assertTrue(histogram.allWords().contains("dark"));
+        Assert.assertTrue(histogram.allWords().contains("and"));
+        Assert.assertTrue(histogram.allWords().contains("stormy"));
+        Assert.assertTrue(histogram.allWords().contains("night"));
+    }
+
+    @Test
     public void shortStory() throws IOException {
         Book book = new Book(Paths.get("src/test/resources/books/ShortStory.txt"));
         //This book has just four distinct words, spread over three lines.
