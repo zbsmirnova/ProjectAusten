bec65823d7fdaf518117c9dec3fdd6f3ea987ef9
TimLavers <tim@grandtestauto.org>
2017-01-30 07:39:20 +1100

    Work in progress

--- src/test/kotlin/pfb/austen/test/BookTest.kt
+++ src/test/kotlin/pfb/austen/test/BookTest.kt
@@ -24,49 +24,48 @@ class BookTest {
         Assert.assertEquals(1, histogram.numberOfTimesGiven("end"));
     }
 
-@Test fun horrorStory() {
-    val book = Book(Paths.get("src/test/resources/books/HorrorStory.txt"))
-    val histogram = book.histogram()
-    val allWords = histogram.allWords()
+    @Test fun horrorStory() {
+        val book = Book(Paths.get("src/test/resources/books/HorrorStory.txt"))
+        val histogram = book.histogram()
+        val allWords = histogram.allWords()
 
-    Assert.assertEquals(7, allWords.size)
+        Assert.assertEquals(7, allWords.size)
 
-    Assert.assertTrue(allWords.contains("it"))
-    Assert.assertTrue(allWords.contains("was"))
-    Assert.assertTrue(allWords.contains("a"))
-    Assert.assertTrue(allWords.contains("and"))
-    Assert.assertTrue(allWords.contains("stormy"))
-    Assert.assertTrue(allWords.contains("night"))
-    Assert.assertTrue(allWords.contains("dark"))
+        Assert.assertTrue(allWords.contains("it"))
+        Assert.assertTrue(allWords.contains("was"))
+        Assert.assertTrue(allWords.contains("a"))
+        Assert.assertTrue(allWords.contains("and"))
+        Assert.assertTrue(allWords.contains("stormy"))
+        Assert.assertTrue(allWords.contains("night"))
+        Assert.assertTrue(allWords.contains("dark"))
 
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("it"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("was"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("a"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("dark"));
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("and"));
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("stormy"));
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("night"));
-}
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("it"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("was"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("a"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("dark"));
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("and"));
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("stormy"));
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("night"));
+    }
 
-@Test
-fun littleBookOfCalmTest() {
-    val book = Book(Paths.get("src/test/resources/books/TheLittleBookOfCalm.txt"))
-    //This book has two lines with several repeated words.
-    //There is also a blank line.
-    //The two non-blank lines end in full stops.
-    val histogram = book.histogram()
-    print(histogram.allWords())
-    Assert.assertEquals(2, histogram.numberOfTimesGiven("this"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("is"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("a"))
-    Assert.assertEquals(3, histogram.numberOfTimesGiven("very"))
-    Assert.assertEquals(2, histogram.numberOfTimesGiven("short"))
-    Assert.assertEquals(2, histogram.numberOfTimesGiven("book"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("we"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("hope"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("that"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("you"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("find"))
-    Assert.assertEquals(1, histogram.numberOfTimesGiven("calming"))
-}
+    @Test
+    fun littleBookOfCalmTest() {
+        val book = Book(Paths.get("src/test/resources/books/TheLittleBookOfCalm.txt"))
+        //This book has two lines with several repeated words.
+        //There is also a blank line.
+        //The two non-blank lines end in full stops.
+        val histogram = book.histogram()
+        Assert.assertEquals(2, histogram.numberOfTimesGiven("this"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("is"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("a"))
+        Assert.assertEquals(3, histogram.numberOfTimesGiven("very"))
+        Assert.assertEquals(2, histogram.numberOfTimesGiven("short"))
+        Assert.assertEquals(2, histogram.numberOfTimesGiven("book"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("we"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("hope"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("that"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("you"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("find"))
+        Assert.assertEquals(1, histogram.numberOfTimesGiven("calming"))
+    }
 }
